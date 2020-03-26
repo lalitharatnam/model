dd770d7a078445455bc5236adbe02eb2f2c16f76
rafi <rafi@r.mshome.net>
2020-03-26 20:49:13 +0530

    my first commit.

---
+++ Demo2/src/main/java/org/cap/boot/MainClass.java
@@ -0,0 +1,11 @@
+package org.cap.boot;
+
+import org.cap.model.Salesclass;
+
+	public class MainClass {
+	public static void main(String[] args)
+	{
+		Salesclass sc=new Salesclass();
+		sc.println();
+	}
+}
---
+++ Demo2/src/main/java/org/cap/model/Salesclass.java
@@ -0,0 +1,11 @@
+package org.cap.model;
+
+public class Salesclass {
+	int arr[]= {23,45,65,87};
+public void println() 
+{
+	for(int i=0;i<arr.length;i++)
+	System.out.println(arr[i]);
+			
+	}
+}
