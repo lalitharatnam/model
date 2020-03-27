498e0bc53c37fce16d9cdc123d045f1e3965eee2
rafi <rafi@r.mshome.net>
2020-03-27 16:49:57 +0530

    this is my sample project

---
+++ Demo2/src/main/java/org/cap/boot/Sample.java
@@ -0,0 +1,14 @@
+package org.cap.boot;
+
+public class Sample {
+ void A() {
+	System.out.println("Love u Amma");
+}
+	public static void main(String[] args)
+	{
+	Sample s=new Sample();
+	s.A();
+		
+	}
+}
+
