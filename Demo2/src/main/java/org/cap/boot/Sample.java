4530f14764949b5bbc93627bb4196f2a8573b384
rafi <rafi@r.mshome.net>
2020-03-28 17:42:59 +0530

    ml

--- Demo2/src/main/java/org/cap/boot/Sample.java
+++ Demo2/src/main/java/org/cap/boot/Sample.java
@@ -1,14 +1,19 @@
 package org.cap.boot;
 
 public class Sample {
+	String m="me and my love";
+	
+	
  void A() {
 	System.out.println("Love u Amma");
+	System.out.println(+m);
+	
+	
 }
 	public static void main(String[] args)
 	{
 	Sample s=new Sample();
 	s.A();
-		
 	}
 }
 
